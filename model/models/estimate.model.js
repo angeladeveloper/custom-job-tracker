609b3b6a0172f92d7b357c5cb9a5b6d24efa67ab
Angela W <squittenplease@gmail.com>
2022-07-18 22:24:13 -0700

    estimate model🧸🏗.
    Still just setting up

--- model/models/estimate.model.js
+++ model/models/estimate.model.js
@@ -9,58 +9,100 @@ Estimate.init(
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
-            autoIncrement: true,
+            autoIncrement: true
         },
         is_new_customer: {
-            type: DataTypes.BOOLEAN,
+            type: DataTypes.BOOLEAN
         },
         estimate_details: {
-            type: DataTypes.TEXT,
+            type: DataTypes.TEXT
         },
         estimate_date: {
             type: DataTypes.DATE,
             allowNull: false,
-            defaultValue: DataTypes.NOW,
+            defaultValue: DataTypes.NOW
         },
         is_receiving: {
-            type: DataTypes.BOOLEAN,
+            type: DataTypes.BOOLEAN
         },
         is_pickup: {
-            type: DataTypes.BOOLEAN,
+            type: DataTypes.BOOLEAN
         },
         is_packing: {
-            type: DataTypes.BOOLEAN,
+            type: DataTypes.BOOLEAN
         },
         is_delivery: {
-            type: DataTypes.BOOLEAN,
+            type: DataTypes.BOOLEAN
         },
         is_white_glove: {
-            type: DataTypes.BOOLEAN,
+            type: DataTypes.BOOLEAN
         },
         is_other: {
-            type: DataTypes.BOOLEAN,
+            type: DataTypes.BOOLEAN
         },
         other_type_details: {
-            type: DataTypes.STRING,
+            type: DataTypes.STRING
         },
         customer_id: {
             type: DataTypes.INTEGER,
             references: {
                 model: 'customer',
-                key: 'id',
-            },
+                key: 'id'
+            }
         },
         estimated_values: {
-            type: DataTypes.STRING,
+            type: DataTypes.STRING
         },
         location_type: {
-            type: DataTypes.STRING,
-        },
-        has_dock: {
-            type: DataTypes.STRING,
+            type: DataTypes.STRING
         },
         needs_delivery_service: {
-            type: DataTypes.BOOLEAN,
+            type: DataTypes.BOOLEAN
+        },
+        pickup_zipcode: {
+            type: DataTypes.STRING
+        },
+        shipping_zipcode: {
+            type: DataTypes.STRING
+        },
+        final_destination_zipcode: {
+            type: DataTypes.STRING
+        },
+        pickup_street_address: {
+            type: DataTypes.STRING
+        },
+        shipping_street_address: {
+            type: DataTypes.STRING
+        },
+        final_destination_street_address: {
+            type: DataTypes.STRING
+        },
+        pickup_city: {
+            type: DataTypes.STRING
+        },
+        shipping_city: {
+            type: DataTypes.STRING
+        },
+        final_destination_city: {
+            type: DataTypes.STRING
+        }, 
+        pickup_state: {
+            type: DataTypes.STRING
+        },
+        shipping_state: {
+            type: DataTypes.STRING
+        },
+        final_destination_state: {
+            type: DataTypes.STRING
+        }, 
+        referral: {
+            type: DataTypes.STRING
+        }, 
+        timeframe_needed: {
+            type: DataTypes.STRING
+        }, 
+        more_information_needed: {
+            type: DataTypes.STRING
         },
     },
     {
@@ -68,7 +110,7 @@ Estimate.init(
         timestamps: false,
         freezeTableName: true,
         underscored: true,
-        modelName: 'project',
+        modelName: 'project'
     }
 );
 
